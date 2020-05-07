a8cfc4a2bf2d94eae91cb7d67fe930a8b6516c1e
hardik dhiman <hardikdhiman.hd6@gmail.com>
2020-05-07 21:02:20 +0530

    update AuthenticationService

--- technical-service/src/main/java/com/upgrad/technical/service/business/AuthenticationService.java
+++ technical-service/src/main/java/com/upgrad/technical/service/business/AuthenticationService.java
@@ -21,11 +21,34 @@ public class AuthenticationService {
     @Autowired
     private PasswordCryptographyProvider CryptographyProvider;
 
-    @Transactional(propagation = Propagation.REQUIRED)
-    public UserAuthTokenEntity authenticate(final String username, final String password) throws AuthenticationFailedException {
-        UserEntity userEntity = userDao.getUserByEmail(username);
+        @Transactional(propagation = Propagation.REQUIRED)
+        public UserAuthTokenEntity authenticate(final String username, final String password) throws AuthenticationFailedException {
+            UserEntity userEntity = userDao.getUserByEmail(username);
+            if (userEntity == null) {
+                throw new AuthenticationFailedException("ATH-001", "User with email not found");
+            }
 
-        final String encryptedPassword = CryptographyProvider.encrypt(password, userEntity.getSalt());
+            final String encryptedPassword = CryptographyProvider.encrypt(password, userEntity.getSalt());
+            if (encryptedPassword.equals(userEntity.getPassword())) {
+                JwtTokenProvider jwtTokenProvider = new JwtTokenProvider(encryptedPassword);
+                UserAuthTokenEntity userAuthTokenEntity = new UserAuthTokenEntity();
+                userAuthTokenEntity.setUser(userEntity);
+                final ZonedDateTime now = ZonedDateTime.now();
+                final ZonedDateTime expiresAt = now.plusHours(8);
+
+                userAuthTokenEntity.setAccessToken(jwtTokenProvider.generateToken(userEntity.getUuid(), now, expiresAt));
+
+                userAuthTokenEntity.setLoginAt(now);
+                userAuthTokenEntity.setExpiresAt(expiresAt);
+
+                userDao.createAuthToken(userAuthTokenEntity);
+
+                userDao.updateUser(userEntity);
+                userEntity.setLastLoginAt(now);
+                return userAuthTokenEntity;
+            } else {
+                throw new AuthenticationFailedException("ATH-002", "Password failed");
+            }
 
     }
 }
