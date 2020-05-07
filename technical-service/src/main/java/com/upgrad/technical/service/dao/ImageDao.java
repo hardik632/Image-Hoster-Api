6196d6538177a09921f3597f70ccecbbab418ad3
hardik dhiman <hardikdhiman.hd6@gmail.com>
2020-05-07 20:20:20 +0530

    update imageDao

--- technical-service/src/main/java/com/upgrad/technical/service/dao/ImageDao.java
+++ technical-service/src/main/java/com/upgrad/technical/service/dao/ImageDao.java
@@ -1,33 +1,39 @@
-//package com.upgrad.technical.service.dao;
-//
-//import com.upgrad.technical.service.entity.ImageEntity;
-//import com.upgrad.technical.service.entity.UserAuthTokenEntity;
-//import org.springframework.stereotype.Repository;
-//
-//import javax.persistence.EntityManager;
-//import javax.persistence.NoResultException;
-//import javax.persistence.PersistenceContext;
-//
-//@Repository
-//public class ImageDao {
-//
-//    @PersistenceContext
-//    private EntityManager entityManager;
-//
-//    public ImageEntity createImage(ImageEntity imageEntity) {
-//        entityManager.persist(imageEntity);
-//    }
-//
-//    public UserAuthTokenEntity getUserAuthToken(final String accesstoken) {
-//    }
-//
-//    public ImageEntity getImage(final String imageUuid) {
-//    }
-//
-//    public ImageEntity getImageById(final long Id) {
-//    }
-//
-//    public ImageEntity updateImage(final ImageEntity imageEntity) {
-//
-//    }
-//}
+package com.upgrad.technical.service.dao;
+
+import com.upgrad.technical.service.entity.ImageEntity;
+import com.upgrad.technical.service.entity.UserAuthTokenEntity;
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.EntityManager;
+import javax.persistence.NoResultException;
+import javax.persistence.PersistenceContext;
+
+@Repository
+public class ImageDao {
+
+    @PersistenceContext
+    private EntityManager entityManager;
+
+    public ImageEntity createImage(ImageEntity imageEntity) {
+        entityManager.persist(imageEntity);
+        return imageEntity;
+    }
+
+    public UserAuthTokenEntity getUserAuthToken(final String accesstoken) {
+        return entityManager.createNamedQuery("userAuthTokenByAccessToken", UserAuthTokenEntity.class).setParameter("accessToken", accesstoken).getSingleResult();
+    }
+
+    public ImageEntity getImage(final String imageUuid) {
+        return entityManager.createNamedQuery("imageEntityByUUID",ImageEntity.class).setParameter("imageUuid",imageUuid).getSingleResult();
+    }
+
+    public ImageEntity getImageById(final long Id) {
+        return entityManager.createNamedQuery("ImageEntityById",ImageEntity.class).setParameter("id",Id).getSingleResult();
+    }
+
+    public ImageEntity updateImage(final ImageEntity imageEntity) {
+        entityManager.merge(imageEntity);
+        return imageEntity;
+
+    }
+}
